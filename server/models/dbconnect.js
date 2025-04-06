6973be1a48e9af82a2a0ab9b6d96eccc19f50254
Jagrat Phugat <151434789+jagrat-phugat@users.noreply.github.com>
2025-04-06 19:12:58 +0530

    Update dbconnect.js

--- server/models/dbconnect.js
+++ server/models/dbconnect.js
@@ -1,8 +1,9 @@
 const mongoose = require("mongoose");
+require("dotenv").config();
 
 const connectDB = async () => {
   try {
-    await mongoose.connect("mongodb://127.0.0.1:27017/CodeIDE");
+    await mongoose.connect("mongodb://127.0.0.1:27017/CodeIDE" || process.env.MONGODB_URL);
     console.log("DB connected successfully");
   } catch (err) {
     console.error("DB connection failed:", err);
