8ee4b070ee4cefd7133a76e893001d50785d45e8
Jagrat Phugat <151434789+jagrat-phugat@users.noreply.github.com>
2025-04-06 22:06:29 +0530

    Update app.js

--- server/app.js
+++ server/app.js
@@ -11,7 +11,8 @@ const userRouter = require('./routes/user')
 
 const app = express()
 
-app.set('views', path.join(__dirname, 'dist'))
+app.use(express.static(path.join(__dirname, 'dist')));
+app.set('views', path.join(__dirname, 'views'))
 app.set('view engine', 'ejs')
 
 app.use(logger('dev'))
