0b3c0aab0f08e240f18421bb7fbf3995d63ff52b
Jagrat Phugat <151434789+jagrat-phugat@users.noreply.github.com>
2025-04-06 19:44:01 +0530

    Update app.js

--- server/app.js
+++ server/app.js
@@ -34,7 +34,7 @@ app.use(function(req, res, next) {
     res.locals.error = req.app.get('env') === 'development' ? err : {};
   
     res.status(err.status || 500);
-    res.render('error');
+    res.send('error');
   });
 
   
