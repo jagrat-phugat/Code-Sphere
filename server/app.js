768eff3318e763a89e1a441227ced4e02a808acd
Jagrat Phugat <151434789+jagrat-phugat@users.noreply.github.com>
2025-04-06 19:51:25 +0530

    Update app.js

--- server/app.js
+++ server/app.js
@@ -20,7 +20,9 @@ app.use(express.urlencoded({extended:false}))
 app.use(cookieParser())
 app.use(express.static(path.join(__dirname, 'public')))
 
-app.use(cors())
+app.use(cors({
+    origin:"https://code-sphere-frontend-hmpi.onrender.com"
+}))
 
 app.use('/', indexRouter)
 app.use('/users', userRouter)
