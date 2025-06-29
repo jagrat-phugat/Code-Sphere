b47a626650bb2042b8b37c99d3c03105a6a4e528
Jagrat Phugat <151434789+jagrat-phugat@users.noreply.github.com>
2025-06-29 22:33:30 +0530

    Update app.js

--- server/app.js
+++ server/app.js
@@ -22,7 +22,7 @@ app.use(cookieParser())
 app.use(express.static(path.join(__dirname, 'public')))
 
 app.use(cors({
-    origin:"https://code-sphere-frontend-hmpi.onrender.com"
+    origin:"*"
 }))
 app.use('/assets', express.static(path.join(__dirname, 'dist/assets'), {
   setHeaders: (res, filePath) => {
